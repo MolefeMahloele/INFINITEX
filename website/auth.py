cd1478cd809ebfd4aeda22a90b7a7cb38c0b7755
Molefe Mahloele <molefemahloele@gmail.com>
2021-12-23 12:24:49 -0800

    initial commit

---
+++ website/auth.py
@@ -0,0 +1,52 @@
+# import re
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return render_template('home.html')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        first_name = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(first_name) < 2:
+            flash('Name must be greater than 1 character', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match', category='error')
+        elif len(password1) < 8:
+            flash('Name must be greater than 7 characters', category='error')
+        else:
+            # Add user to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account Created Successfully!', category='success')
+            return redirect(url_for('views.home'))
+
+    return render_template('sign_up.html')
+
+# Custom Error Pages
+@auth.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# Internal Server Error
+@auth.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
